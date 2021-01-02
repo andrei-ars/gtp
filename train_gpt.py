1bc8d0532af87bde4fa7d7cb6d8bd0b0676dbc71
Andrei <z@sp.sp>
2021-01-02 14:35:37 +0300

    1

--- train_gpt.py
+++ train_gpt.py
@@ -1,3 +1,9 @@
+"""
+https://github.com/minimaxir/gpt-2-simple
+https://colab.research.google.com/drive/1VLG8e7YSEwypxU-noRNhsv5dW4NfTGce#scrollTo=aeXshJM-Cuaf
+https://minimaxir.com/2019/09/howto-gpt2/
+"""
+
 import gpt_2_simple as gpt2
 import os
 import requests
@@ -8,14 +14,15 @@ if not os.path.isdir(os.path.join("models", model_name)):
     gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
 
 
-file_name = "shakespeare.txt"
-if not os.path.isfile(file_name):
-    url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
-    data = requests.get(url)
-    
-    with open(file_name, 'w') as f:
-        f.write(data.text)
-    
+#file_name = "shakespeare.txt"
+#if not os.path.isfile(file_name):
+#    url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+#    data = requests.get(url)
+#    
+#    with open(file_name, 'w') as f:
+#        f.write(data.text)
+
+file_name = "train.txt"
 
 sess = gpt2.start_tf_sess()
 #gpt2.finetune(sess,
@@ -26,12 +33,12 @@ sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
               dataset=file_name,
               model_name='124M',
-              steps=1000,
+              steps=100,
               restore_from='fresh',
               run_name='run1',
-              print_every=10,
-              sample_every=200,
-              save_every=500
+              print_every=1,
+              sample_every=20,
+              save_every=50
               )
 
 gpt2.generate(sess)
